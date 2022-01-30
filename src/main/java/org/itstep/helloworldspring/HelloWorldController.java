1f7d8a41e08a07430d1faf579fdbc874bec8b7e3
KirillovItstep <kirillov.malp@gmail.com>
2022-01-30 16:40:54 +0300

    Hello World from Spring

---
+++ src/main/java/org/itstep/helloworldspring/HelloWorldController.java
@@ -0,0 +1,17 @@
+package org.itstep.helloworldspring;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+    @RequestMapping
+    public String helloWorld(){
+        return "Hello World from Spring Boot";
+    }
+
+    @RequestMapping("/bye")
+    public String bye(){
+        return "Good bye from Spring Boot";
+    }
+}
